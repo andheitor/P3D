e889a1aa18988c9fbcff4c6d004cb7c8b4d6a77e
Otavio Lipari <otavio@welcomemkt.com.br>
2012-07-26 01:49:34 -0300

    inicial

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module P3D
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.assets.precompile += ['bootstrap.css']
     config.assets.precompile += ['bootstrap.js']
 
