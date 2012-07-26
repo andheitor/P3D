9ba06312ae8cfe6f7558bf1571095c80ae0fe98e
Otavio Lipari <otavio@welcomemkt.com.br>
2012-07-26 01:24:09 -0300

    inicial

--- app/models/admin/conteudo.rb
+++ app/models/admin/conteudo.rb
@@ -2,9 +2,11 @@
 class Admin::Conteudo < ActiveRecord::Base
   attr_accessible :iframe, :link, :menu, :texto_curto, :texto_longo, :titulo, :localizacao, :imagem,:translations_attributes, :imagem_file_name,:imagem_content_type,:imagem_file_size, :imagem_updated_at
   validates_presence_of :menu, :titulo
-  has_attached_file :imagem , 
+  has_attached_file :imagem ,
+                     :default_url => "images/missing.jpg",
+                     :default_style => :medium,   
                     :styles => { :medium => "108x88>", :thumb => "100x100>" }
-                    :default_url => '/images/missing_thumb.png'
+                    
   #has_attached_file :imagem , :styles => { :medium => "108x88>" }
   translates :titulo, :texto_curto, :texto_longo
   accepts_nested_attributes_for :translations
